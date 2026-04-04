13213de817b7f6e2adf214abaa16bb691b1bb40a
moody <13608424089@163.com>
2026-04-04 22:43:22 +0800

    fix: watermark.worker 类型错误

--- src/pages/water_mark/watermark.worker.ts
+++ src/pages/water_mark/watermark.worker.ts
@@ -2,7 +2,9 @@
  * 暗水印处理 Worker
  * 逻辑：在像素的 R 通道最低位嵌入信息
  */
-self.onmessage = (e: MessageEvent) => {
+const ctx: Worker = self as any;
+
+ctx.onmessage = (e: MessageEvent) => {
   const { imageData, text } = e.data;
   const data = imageData.data;
 
@@ -18,15 +20,12 @@ self.onmessage = (e: MessageEvent) => {
     if (bitIndex < textBinary.length) {
       const bit = parseInt(textBinary[bitIndex]);
       // 强制将最低位设为 bit (0 或 1)
-      // 如果原值是 254(二进制 ...1110)，bit是1 -> 变成 255
-      // 如果原值是 255(二进制 ...1111)，bit是0 -> 变成 254
       data[i] = (data[i] & 0xfe) | bit;
       bitIndex++;
     } else {
-      // 标记结束符：连续 8 个 0 的 R 通道 LSB 可能表示结束（此处仅为简化演示）
       break;
     }
   }
 
-  self.postMessage({ imageData }, [imageData.data.buffer]);
+  ctx.postMessage({ imageData }, [imageData.data.buffer]);
 };
